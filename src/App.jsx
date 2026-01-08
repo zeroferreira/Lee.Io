78a681596750b8308b9cb9dea18a7421bf040657
Mario Ferreira <zeroferreira@MacBook-Pro-de-Zero.local>
2026-01-08 15:43:13 -0600

    fix: update google client id to newly generated credential

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { ProfileScreen } from './components/ProfileScreen';
 import { Notification } from './components/Notification';
 import useDrivePicker from 'react-google-drive-picker';
 
-const GOOGLE_CLIENT_ID = "900762317960-j8481f1d3vd3gvl1e1qg98n024jp70ci.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "900762317960-g972j1tn884t9pgbl9e6f3dhd857ef0k.apps.googleusercontent.com";
 
 function AppContent() {
   const [showIntro, setShowIntro] = useState(true);
